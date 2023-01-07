731a81c5e3a53f87bd32e2be81f6236937abd745
C4skg <2269498386@qq.com>
2023-01-07 22:53:44 +0800

    commit

--- core/server.py
+++ core/server.py
@@ -1,10 +1,15 @@
 from flask import Flask,render_template,url_for;
-
+import uuid;
 app = Flask(__name__,template_folder='../templates',static_folder='../static')
 
 @app.route('/')
 def main():
     return render_template('index.html');
+
+
+@app.errorhandler(404)
+def error_404(error_info):
+    return render_template("404.html",id=uuid.uuid4()),404;
 
 def run(port=8080,debug:bool = False):
     app.run('127.0.0.1',port,debug=debug);
