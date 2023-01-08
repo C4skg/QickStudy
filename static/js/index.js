68db4f31ac00b1c9c6cd5bfecf6e28fc5bb18609
C4skg <2269498386@qq.com>
2023-01-08 10:27:57 +0800

    commit

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@ window.onload = function(){
     'use strict';
     $(".change").click(function(){
         var dark = $('html').attr('data-user-color-scheme');
-        if(dark === undefined){
+        if(dark === undefined || dark == ""){
             $('html').attr("data-user-color-scheme","dark")
         }else{
             $('html').removeAttr('data-user-color-scheme')
