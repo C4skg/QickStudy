9b64d0e52f0ec719effbcb797580bbccb55fb244
C4skg <2269498386@qq.com>
2023-01-26 19:10:58 +0800

    commit

--- static/js/index.js
+++ static/js/index.js
@@ -4,6 +4,12 @@ window.onload = function(){
     (function(){
         changeMode();
         checkDev();
+
+        //
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
+        })
     })();
 
     $(".modeButton").click(function(){
