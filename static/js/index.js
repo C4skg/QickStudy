612f614263e0d55a28f8ce3b8c93bf36059b6a79
C4skg <2269498386@qq.com>
2023-02-19 19:26:57 +0800

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -106,18 +106,16 @@ var ScrollPos = function(){
     };
 }
 
-Array.prototype.remove = function(...argv){
-    if(argv.length > 1){
-        argv.forEach(element => {
-            this.remove(element);
-        });
+Array.prototype.remove = function(...args){
+    while(args.length > 1){
+        this.removes(args.pop());
     }
-    let index = this.indexOf(argv[0]);
+    let index = this.indexOf(args[0]);
     while(index > -1){
-        this.splice(index,1);
-        index = this.indexOf(argv[0]);
+        this.splice(index, 1);
+        index = this.indexOf(args[0]);
     }
-    return this; //会影响本身，建议复制
+    return this; // 影响本身，建议复制
 }
 String.prototype.decode = function(){
     if (this.length === 0) {
