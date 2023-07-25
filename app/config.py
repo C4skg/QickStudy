fe49863b3e42fc82e82684c71f9423e7f4b64c49
C4skg <2269498386@qq.com>
2023-07-25 21:33:34 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -17,7 +17,11 @@ class Config:
     COMPRESS_LEVEL = 9
 
     #SQL
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@127.0.0.1:3306/QickStudy?charset=utf8mb4'
+    SQL_USER = "root"
+    SQL_PASSWORD = "123456"
+    SQL_PORT = "3306"
+    SQL_SCHEMA = "QickStudy"
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{SQL_USER}:{SQL_PASSWORD}@127.0.0.1:{SQL_PORT}/{SQL_SCHEMA}?charset=utf8mb4'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
 
