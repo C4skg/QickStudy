65091ce52b69b796c87c5487da239fe4041bfaee
C4skg <2269498386@qq.com>
2023-07-26 10:59:38 +0800

    update
    backdoor

--- app/backdoor/requestion.py
+++ app/backdoor/requestion.py
@@ -4,20 +4,26 @@ from flask_login import login_required
 
 from . import php
 
-
-
 @php.before_request
 def before_request():
     if current_user.is_authenticated:
         pass;
     else:
+        print(current_user)
+
         return (
             '''
                 I hope you can find the leak on my website,
-                and I will give you a flag as a reward
+                and I will give you a flag as a reward;
+                <br>
+                Ok, so the first step is login,after that,you can refresh this page;
             '''
         )
     
 @php.route('/ok.php')
 def ok():
-    return 'phpinfo();'
+    return 'try a?'
+
+@php.route('/backdoor.asp')
+def ok_Aps():
+    return 'IIS?';  
