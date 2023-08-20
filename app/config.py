fb03c0b670c5bc47b4f411d07ddb9aba5319645a
C4skg <2269498386@qq.com>
2023-08-21 01:20:19 +0800

    Update config.py
    脱敏

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ class Config:
     MAIL_SERVER='smtp.qq.com'
     MAIL_PORT='465'
     MAIL_USERNAME='C4skg@qq.com'
-    MAIL_PASSWORD='fhxgqmfgnryliabf'  #!可在邮箱设置中获取
+    MAIL_PASSWORD='xxxxxxxxxx'  #!可在邮箱设置中获取
     MAIL_USE_SSL=True
     MAIL_USE_TLS=False
     MAIL_DEBUG = False
